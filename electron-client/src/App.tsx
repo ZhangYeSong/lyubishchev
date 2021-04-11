0eb3f651a512aa7229b6a1a46c4194acb659c6c1
ZhangYeSong <17602105720@163.com>
2021-04-11 17:48:20 +0800

    点击事件

--- electron-client/src/App.tsx
+++ electron-client/src/App.tsx
@@ -3,55 +3,106 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
 import { Input, Button, List } from 'antd';
+import {Work} from './Work';
+import  moment from 'moment';
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+moment.locale('zh-cn');
 
-const Hello = () => {
-  return (
-    <div className="Hello">
-      <h1>柳比歇夫计时器</h1>
-      <Input size="large" placeholder="工作"/><br/>
-      <Button
-        className="operate_button"
-        type="primary"
-        shape="round"
-        size="large" block>
-        开始
-      </Button>
-      <List
-        className="work-list"
-        size="large"
-        header={<div>Header</div>}
-        footer={<div>Footer</div>}
-        bordered
-        dataSource={data}
-        renderItem={item => <List.Item>{item}</List.Item>}
-      />
-    </div>
-  );
-};
+interface MainPagerState {
+  workTitle: string,
+  working: boolean,
+  works: Work[],
+  startTime: number
+}
+
+class MainPager extends React.Component<Object, MainPagerState> {
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      workTitle: "",
+      working: false,
+      works: [],
+      startTime: moment().valueOf()
+    };
+  }
+
+  private getButtonText = () => {
+    let working = this.state.working;
+    return working? "停止":"开始";
+  }
+
+  private handleClick = () => {
+    this.setState({
+      working: !this.state.working
+    });
+    if (this.state.working) {
+      let works = this.state.works;
+      let title = this.state.workTitle;
+      if (title.trim().length <= 0) {
+        title = '工作';
+      }
+      let work: Work = {
+        userId: 0,
+        insertTime: moment().valueOf(),
+        content: title,
+        startTime: this.state.startTime,
+        endTime: moment().valueOf()
+      };
+      works.push(work);
+    } else {
+      this.setState({startTime: moment().valueOf()})
+    }
+  }
+
+  private getWorkTimeString = (work: Work) => {
+    return work.content
+      + " "
+      + moment(work.startTime).format("LT")
+      + " - "
+      + moment(work.endTime).format("LT")
+      + " "
+      + moment(work.endTime).diff(moment(work.startTime), "minutes")
+      + "分钟";
+  }
+
+  private onInputChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    this.setState({
+      workTitle: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="Hello">
+        <h1>柳比歇夫计时器</h1>
+        <Input onChange={this.onInputChange}
+               value={this.state.workTitle}
+               size="large" placeholder="工作"/><br/>
+        <Button
+          className="operate_button"
+          onClick={this.handleClick}
+          type="primary"
+          shape="round"
+          size="large" block>
+          {this.getButtonText()}
+        </Button>
+        <List
+          className="work-list"
+          size="large"
+          bordered
+          dataSource={this.state.works}
+          renderItem={item => <List.Item>{this.getWorkTimeString(item)}</List.Item>}
+        />
+      </div>
+    );
+  }
+}
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={MainPager} />
       </Switch>
     </Router>
   );
---
+++ electron-client/src/Work.ts
@@ -0,0 +1,7 @@
+export interface Work {
+  userId: number;
+  insertTime: number;
+  content: string;
+  startTime: number;
+  endTime: number;
+}
